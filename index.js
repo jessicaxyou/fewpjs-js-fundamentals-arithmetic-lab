4696a915de2a5c8949e2ef8ccf1eed3d7eb6a885
jessica you <jessicaxytan@gmail.com>
2021-04-29 10:16:39 -0400

    commit tree 2cb1a637341cb398f4dc42cecbdbf31d45906608

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+const num3 = 9
+
+const num4 = 31;
+
+let multiply = (num1*num2);
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = (num4 % num3);
+
+let max = Math.max(5,10,15,20)
